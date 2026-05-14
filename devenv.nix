33c872b07a75a08f0349dff999579d403bd10c43
Joao Fernandes <joao.fernandes@ist.utl.pt>
2026-05-14 01:00:41 +0100

    add devenv setup

---
+++ devenv.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  packages = with pkgs; [
+    yubikey-manager # ykman
+    sops
+    age-plugin-yubikey
+    ssh-to-age
+    openssh # ssh-keygen for host-key generation
+    nixos-anywhere
+    nixos-rebuild
+  ];
+}
